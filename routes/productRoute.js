ebbfe465338c3fb80d5077d56c93e88a832a087e
pratik511 <pp05111999@gmail.com>
2022-07-28 18:31:46 +0530

    make it better

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -6,7 +6,9 @@ const Router = express.Router();
 
 Router.route("/products").get(getAllProducts);
 Router.route("/products/new").post(createProduct);
-Router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetail);
+Router.route("/product/:id").put(updateProduct);
+Router.route("/product/:id").delete(deleteProduct);
+Router.route("/product/:id").get(getProductDetail);
 
 
 module.exports = Router
