e0b1786820971a739627e0273681911e5e748c66
pratik511 <pp05111999@gmail.com>
2022-07-29 10:41:10 +0530

    make it better11

--- config/database.js
+++ config/database.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
 
-    mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true/* ,useCreateIndex:true */ })
+    mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true ,useCreateIndex:true  })
     .then((data) => { console.log(`Mongodb connected wit server : ${data.connection.host}`); })
-    // .catch((err) => { console.log(err); })
+    .catch((err) => { console.log(err); })
 }
 
 module.exports = connectDatabase;
